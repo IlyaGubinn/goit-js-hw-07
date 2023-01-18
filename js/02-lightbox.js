111bb6f34c12f847b9c969b50538c8840dc6bb1f
IlyaGubinn <ilya.gubin29@gmail.com>
2023-01-18 17:18:45 +0300

    решение второй задачи

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const cardPicctures = creetePicturesCard(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', cardPicctures)
+
+
+function creetePicturesCard(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+         return `<div class="gallery__item"><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description}/>
+</a></div>`
+    }).join("");
+}
+
+new SimpleLightbox('.gallery__item a', { captions: true, captionsData: 'alt', captionDelay:250, });
+
 
 console.log(galleryItems);
